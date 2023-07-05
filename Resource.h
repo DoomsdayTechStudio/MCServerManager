3bfa575385322038cf88cac48a3ab4acd1df59c5
xiewoc <70128845+xiewoc@users.noreply.github.com>
2023-07-05 09:56:20 +0800

    Add files via upload

---
+++ Resource.h
@@ -0,0 +1,7 @@
+#define IDM_EXIT           101
+#define IDM_ABOUT          102 
+#define IDM_HIDEFORGE      103
+#define IDM_SHOWFORGE      104
+#define IDM_HIDEORIGON     105 
+#define IDM_SHOWORIGON     106 
+#define IDI_NOTIFICATIONICON 207
